8ba756b860340ba89adb0404cc108ed52e71c676
Florian Aßmann <florian.assmann@email.de>
2010-05-21 16:07:25 +0200

    Clean ups.
     * os independent slugged track path
     * used paperclips content-type validation
     * read title from id3 tag if not given
     * added playlist association stub to audio
     * removed extra error messages from form partial
     * some minor code simplifications

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -1,47 +1,66 @@
 class Audio < ActiveRecord::Base
 
   has_attached_file :track,
-    :url => "/:class/:id/:basename.:extension",
-    :path => ":rails_root/public/:class/:id/:basename.:extension"
-  
+    :url  => File.join('', %w[ audio :playlist_slug :track_slug.:extension ]),
+    :path => File.join(%w[ :rails_root public audio :playlist_slug :track_slug.:extension ])
+
   belongs_to :created_by, :class_name => 'User'
   belongs_to :updated_by, :class_name => 'User'
-  
+
   validates_presence_of :title
   validates_uniqueness_of :title, :message => 'name already in use'
-  # mp3 MIME types taken from: http://filext.com/file-extension/MP3
-  validates_format_of :track_content_type, 
-                      :with => /(audio|application)\/(x-)?(mpe?g|mp3|mpeg3|mpegaudio)/,
-                      :message => "must be an mp3"
+
+  # MIME types taken from: http://filext.com/
+  validates_attachment_content_type :track,
+      :content_type => /a(?:udio|pplication)\/(?:x-)?mp(?:e?g|3|eg3|egaudio)/
+
+  # TODO replace this stub with association
+  PLAYLIST = 'Playlist'
+  def PLAYLIST.slug; 'playlist' end
+  def playlist; PLAYLIST end
 
   def to_param
-    [id, slug_from_title(title)].join("-")
+    "#{ id }-#{ slug }"
   end
-  
+
   def path
-    movie_pages = Page.find_all_by_class_name("AudioPage")
-    return nil unless movie_pages.size == 1
-    movie_page = movie_pages.first
-    path = [movie_page.url, to_param].join()
+    return if Page.count(:class_name => 'AudioPage') != 1
+    "#{ Page.find_by_class_name('AudioPage').first.url }#{ to_param }"
   end
-  
+
   def description_with_filter
-    if filter
-      f = TextFilter.descendants.find { |tf| tf.filter_name == filter }
-      f.filter(description)
+    if filter = TextFilter.descendants.find { |f| f.filter_name == filter }
+      filter.filter description
     else
       description
     end
   end
-  
-  def url
-    track.url
+
+  delegate :url, :to => :track
+
+  def title
+    string = read_attribute :title
+
+    if string.blank? and string = read_tag(:title)
+      write_attribute :title, string
+    end
+
+    string
   end
-  
-  private
-  
-  def slug_from_title(title)
-    title.downcase.split.join("-")
+
+  def read_tag(name)
+    mp3info.tag[name.to_s] if track.file?
   end
-  
+
+  def slug
+    title.downcase.split * '-'
+  end
+
+  protected
+
+    def mp3info
+      object = track.dirty?? track.queued_for_write[:original] : track
+      @mp3info ||= Mp3Info.new object.path
+    end
+
 end

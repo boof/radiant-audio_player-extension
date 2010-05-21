ab42d264058e5c3f10d3384da8f72b9634cfec0d
Florian Aßmann <florian.assmann@email.de>
2010-05-21 13:48:10 +0200

    Lots...
     * cleaned up public folder
     * added jplayer javascript
     * renamed public audio_player to audio-player
     * moved AudioPlayerExtension to AudioPlayer::Extension to expose
       configuration methods in AudioPlayer module
     * aliased AudioPlayer::Extension as AudioPlayerExtension
     * added translations for form partial and remove view
     * some view tweaks...

--- app/helpers/admin/audio_helper.rb
+++ app/helpers/admin/audio_helper.rb
@@ -1,22 +1,29 @@
 module Admin::AudioHelper
-  
-  def admin_player_params(audio_track)
 
-    if config = AudioPlayerConfig.find_by_name("Admin")
-      player_params = config.parameters_for_player
-    else
-      player_params = ["autostart=no","loop=no"]
-    end
+  def render_tracks(object, locals = nil)
+    locals = { :level => 1, :simple => false } unless Hash === locals
 
-    if audio_track.class == Audio
-      player_params += ["playerID=#{audio_track.id}","soundFile=#{audio_track.track.url}"]
-    else
-      player_params += ["playerID=example","soundFile=#{audio_track}"]
-    end
-
-    player_params.join("&amp;")
+    render Array === object ?
+        { :partial => 'track', :collection => object, :locals => locals } :
+        { :partial => 'track', :object => object, :locals => locals }
   end
-  
-  
-  
+
+end
+
+__END__
+def admin_player_params(audio_track)
+
+  if config = AudioPlayerConfig.find_by_name("Admin")
+    player_params = config.parameters_for_player
+  else
+    player_params = ["autostart=no","loop=no"]
+  end
+
+  if audio_track.class == Audio
+    player_params += ["playerID=#{audio_track.id}","soundFile=#{audio_track.track.url}"]
+  else
+    player_params += ["playerID=example","soundFile=#{audio_track}"]
+  end
+
+  player_params.join("&amp;")
 end
--- audio_player_extension.rb
+++ audio_player_extension.rb
@@ -1,17 +1,21 @@
-class AudioPlayerExtension < Radiant::Extension
+module AudioPlayer
+  class Extension < Radiant::Extension
 
-  version '0.1.0'
-  description 'Easily upload mp3 files, and embed them on your site with jPlayer.'
-  url 'http://github.com/boof/radiant-audio_player-extension/tree/master'
+    version '0.1.0'
+    description 'Easily upload mp3 files, and embed them on your site with jPlayer.'
+    url 'http://github.com/boof/radiant-audio_player-extension/tree/master'
 
-  def activate
-    ActiveSupport::Inflector.inflections do |inflect|
-      inflect.uncountable 'audio'
-    end if 'audio'.pluralize != 'audio'
+    def activate
+      set_audio_uncountable if 'audio'.pluralize != 'audio'
+      Page.send :include, AudioTags
+      tab('Content') { add_item 'Audio', '/admin/audio' }
+    end
 
-    Page.send :include, AudioTags
+    def set_audio_uncountable
+      ActiveSupport::Inflector.inflections { |inf| inf.uncountable 'audio' }
+    end
 
-    tab('Content') { add_item 'Audio', '/admin/audio' }
   end
-
 end
+
+AudioPlayerExtension = AudioPlayer::Extension

dc8297d9a59fe1af60e639ee8c77c089a34271e4
Florian Aßmann <florian.assmann@email.de>
2010-05-21 21:29:28 +0200

    Useless assign of isCurrent.

--- public/javascripts/audio-player/audio-player.js
+++ public/javascripts/audio-player/audio-player.js
@@ -11,11 +11,11 @@
 
         playerControls
           .click(function(e) {
-            var self = $(this), src = self.attr('href'),
-                isCurrent = player.getData('diag.src') == src;
+            var self = $(this), src = self.attr('href');
 
             playerControls.removeClass('active');
-            if (!isCurrent) {
+
+            if (player.getData('diag.src') != src) {
               player.setFile(src);
               player.play();
               self.addClass('active');
